fd966f9a74501b7f38a7b2c474e024f137f123df
Rajan Kumar <raorajan9576@gmail.com>
2024-05-06 12:48:05 +0530

    Profile pic added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import common from "../styles/Common.module.css";
 import { applyScrollReveal } from "../config/scrollRevealConfig";
+import Typewriter from "typewriter-effect";
 
 function Home() {
   useEffect(() => {
@@ -11,7 +12,33 @@ function Home() {
   return (
     <>
       <div id="home" style={{ paddingTop: "100px" }} className="scrollview">
-        <div className={common.boxes}></div>
+        <div className={common.boxes}>
+          <div className={styles.aboutContainer}>
+            <div className={styles.content}>
+              <div className={styles.aboutText}>
+                <h2>Hello 👋.</h2>
+                <p>My name is Rajan Kumar.</p>
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Full Stack Developer.",
+                      "Tech Enthusiast.",
+                      "Life Long Learner",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+                <button>Resume</button>
+              </div>
+            </div>
+            <div className={styles.imageContainer}>
+              <div className={styles.imageWithCircle}>
+                <img src="/profile.png" alt="Rajan Kumar" width={200} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
