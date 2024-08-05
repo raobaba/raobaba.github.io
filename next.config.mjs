5c5d62bda54743d92923d4a54084515ecbe5cc25
Rajan Kumar <raorajan9576@gmail.com>
2024-08-06 00:32:57 +0530

    Updates

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  trailingSlash: true,
-  basePath: "/Portfolio",
-  output: "export",
-  reactStrictMode: true,
-};
-
-export default nextConfig;
